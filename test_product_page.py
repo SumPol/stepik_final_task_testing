5b95a3e1d1488f43c6adc483b7ba31958ed62d37
Polina <polya_star95@mail.ru>
2021-03-07 23:50:00 +0300

    tests marked

--- test_product_page.py
+++ test_product_page.py
@@ -75,13 +75,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_add_to_basket()
     page.should_disappeared_success_message()
 
-@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
